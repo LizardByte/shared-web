16fb0030d45717c50bbd391e5f7a5d56348a4fc8
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-09-18 19:29:02 -0400

    fix: refine Dockle migration

---
+++ readthedocs_post_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "baseurl: projects/shared-web/${READTHEDOCS_VERSION}/jekyll" >> examples/jekyll/_config.yml
+pushd examples/jekyll
+npm ci
+npm run build
+popd
