279ca0dddf926a24c06c30edc027ead9c2d84985
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-09-18 19:47:30 -0400

    fix: harden Read the Docs npm install

--- readthedocs_post_build.sh
+++ readthedocs_post_build.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 echo "baseurl: projects/shared-web/${READTHEDOCS_VERSION}/jekyll" >> examples/jekyll/_config.yml
 pushd examples/jekyll
-npm ci
+npm ci --ignore-scripts
 npm run build
 popd
